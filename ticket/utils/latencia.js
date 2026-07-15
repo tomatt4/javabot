fee393c131a00f63fb2481dabab57bfdaf8208ef
MATTZADDAS <santosmiguelalves6@gmail.com>
2026-07-14 22:13:58 -0300

    Delete ticket/utils/latencia.js

--- ticket/utils/latencia.js
+++
@@ -1,1 +0,0 @@
-const {SlashCommandBuilder}=require('discord.js');module.exports={data:new SlashCommandBuilder().setName('sohakari').setDescription('so hakari'),async execute(c,i){await i.reply('so hakari');}};
