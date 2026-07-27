0643d93379727af8c93b14bc41a3c17138223fac
MATTZADDAS <santosmiguelalves6@gmail.com>
2026-07-26 23:04:44 -0300

    Update tellonym.js

--- ticket/commands/tellonym.js
+++ ticket/commands/tellonym.js
@@ -16,22 +16,20 @@ module.exports = {
         .setDescription('Abas para envio de Tellonym anônimo ou público.'),
 
     async execute(interaction) {
-        const container = new ContainerBuilder()
-            .setAccentColor(0x5865F2) // Cor da barra lateral do container
-            .addComponents(
-                // A imagem agora vem primeiro (topo)
-                new MediaGalleryBuilder()
-                    .addItems(
-                        new MediaGalleryItemBuilder()
-                            .setURL('https://i.postimg.cc/vHx6xSgZ/image0.png')
-                    ),
-                // O texto vem logo abaixo da imagem
-                new TextDisplayBuilder()
-                    .setContent(
-                        '# 📩 Tellonym CDV\n' +
-                        'Clique no botão abaixo para abrir as opções e enviar sua mensagem.'
-                    )
-            );
+        const container = new ContainerBuilder();
+        container.setAccentColor(0x5865F2);
+        container.addComponents(
+            new MediaGalleryBuilder()
+                .addItems(
+                    new MediaGalleryItemBuilder()
+                        .setURL('https://i.postimg.cc/vHx6xSgZ/image0.png')
+                ),
+            new TextDisplayBuilder()
+                .setContent(
+                    '# 📩 Tellonym CDV\n' +
+                    'Clique no botão abaixo para abrir as opções e enviar sua mensagem.'
+                )
+        );
 
         const actionRow = new ActionRowBuilder()
             .addComponents(
