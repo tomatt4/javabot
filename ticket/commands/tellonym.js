84072f0c6bab3fda76ae852c9c50390c71bebbcf
MATTZADDAS <santosmiguelalves6@gmail.com>
2026-07-26 23:13:26 -0300

    Update tellonym.js

--- ticket/commands/tellonym.js
+++ ticket/commands/tellonym.js
@@ -3,7 +3,6 @@ const {
     ActionRowBuilder, 
     ButtonBuilder, 
     ButtonStyle,
-    ContainerBuilder,
     TextDisplayBuilder,
     MediaGalleryBuilder,
     MediaGalleryItemBuilder,
@@ -16,31 +15,36 @@ module.exports = {
         .setDescription('Abas para envio de Tellonym anônimo ou público.'),
 
     async execute(interaction) {
-        const container = new ContainerBuilder();
-        container.setAccentColor(0x5865F2);
-        container.addComponents(
+
+        const components = [
+
+            // Imagem no topo
             new MediaGalleryBuilder()
                 .addItems(
                     new MediaGalleryItemBuilder()
                         .setURL('https://i.postimg.cc/vHx6xSgZ/image0.png')
                 ),
+
+            // Texto abaixo da imagem
             new TextDisplayBuilder()
                 .setContent(
-                    '# 📩 Tellonym CDV\n' +
-                    'Clique no botão abaixo para abrir as opções e enviar sua mensagem.'
-                )
-        );
+                    '# 📩 Tellonym\n' +
+                    'Envie mensagens anônimas ou públicas para outros usuários.\n\n' +
+                    'Clique no botão abaixo para começar.'
+                ),
 
-        const actionRow = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('btn_abrir_opcoes_tellonym')
-                    .setLabel('Enviar Tellonym')
-                    .setStyle(ButtonStyle.Primary)
-            );
+            // Botão no final
+            new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('btn_abrir_opcoes_tellonym')
+                        .setLabel('Enviar Tellonym')
+                        .setStyle(ButtonStyle.Primary)
+                )
+        ];
 
         return interaction.reply({
-            components: [container, actionRow],
+            components,
             flags: MessageFlags.IsComponentsV2
         });
     }
