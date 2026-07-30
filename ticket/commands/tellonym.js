76d9014a73faa7172261156170a013102733221d
MATTZADDAS <santosmiguelalves6@gmail.com>
2026-07-30 11:03:21 -0300

    Update tellonym.js

--- ticket/commands/tellonym.js
+++ ticket/commands/tellonym.js
@@ -28,7 +28,6 @@ module.exports = {
                     'Bem vindo ao sistema de **Tellonym** da CDV! O Tellonym foi criado no intuito de trazer entretenimento e diversão pro servidor, com você podendo enviar mensagens **anônimas** ou **públicas**!\n\n' +
                     'Qualquer pergunta, abra Ticket. Clique no botão abaixo para começar!'
                 )
-                .setFooter('Sistema Tellonym feito por Salva')
         );
                 
         // Imagem no meio
